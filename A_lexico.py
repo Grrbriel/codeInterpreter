f0a189742b056ead1bb0c3d3334df309e8ff21d6
Grrbriel <gabrielggeasy@gmail.com>
2023-11-07 13:51:27 -0300

    Commit inicial

---
+++ A_lexico.py
@@ -0,0 +1,78 @@
+import ply.lex as lex
+
+#Especificação dos tokens
+tokens = [
+       'NUMBER', #numero inteiro
+       'LPAREN', #(
+       'RPAREN', #)
+       'COMMA',  #,
+       'EQUAL',  #=
+       'WORDS'   #Palavras reservadas e verificação de violação
+]
+
+reserved = {'connect':'CONNECT', #conectar pontos no plano
+             'draw':'DRAW', #desenhar forma especificada
+             'square':'SQUARE', #especificar forma
+             'triangle':'TRIANGLE', #especificar forma
+             'rectangle':'RECTANGLE', #especificar forma
+             'size':'SIZE', #especificar tamanho
+             'height':'HEIGHT', #especificar altura
+             'weight':'WEIGHT', #especificar largura
+             'point':'POINT',#especificar ponto no plano
+             'AND':'AND', #condicao AND
+             'start':'START'} #especificar ponto inicial de figura
+
+tokens = tokens + list(reserved.values())
+
+#REGEX DOS TOKENS
+t_EQUAL   = r'\='
+t_LPAREN  = r'\('
+t_RPAREN  = r'\)'
+t_COMMA   = r'\,'
+
+#REGEX de palavras e filtragem de palavras reservadas
+def t_WORDS(t):
+    r'[a-zA-Z]+'
+    t.type = reserved.get(t.value)
+    if (t.type == None):
+        t_error(t)
+    else:
+        return t
+
+#REGEX de numero inteiro
+def t_NUMBER(t):
+    r'\d+'
+    t.value = int(t.value)
+    return t
+
+#REGEX que controla numero de linhas
+def t_newline(t):
+    r'\n+'
+    t.lexer.lineno += len(t.value)
+
+#REGEX dos ignorados (espaços e tabulação)
+t_ignore  = ' \t'
+
+#REGRA de tratamento de caracter ilegal
+def t_error(t):
+    print("Illegal character or Word: '%s'" % t.value)
+    t.lexer.skip(1)
+
+
+#BUILD do analisador lexico
+lexer = lex.lex()
+
+#String teste simulando input do usuário
+input = 'SQUARE connect point(1,1) AND point(2,2) AND point(3,3)\ndraw SQUARE s draw square size = 10 start(4,5)'
+
+
+#INPUT para o analisador
+lexer.input(input)
+
+#TOKENIZACAO
+while True: #PRINT (TYPE, VALUE, LINE_NUMBER, POSITION_IN_STRING)
+    current_token = lexer.token()
+    if not current_token:
+        break      #Fim input
+    print(current_token)
+
---
+++ main.py
@@ -0,0 +1,29 @@
+import tkinter as tk
+import ply.lex as lex
+import cProfile
+
+root = tk.Tk()
+root.title('Tkinter Window Demo')
+root.mainloop()
+
+print ("EU amo pizza!")
+
+
+###ANALISADOR LEXICO
+tokens = ['a_par', #(
+          'f_par', #)
+          'op_vir', #,
+          'op_igual', #=
+          'op_and', #AND
+          'op_eol'] #;
+
+reserverd = {'connect':'connect', #conectar pontos no plano
+             'draw':'draw', #desenhar forma especificada
+             'square':'square', #especificar forma
+             'triangle':'triangle', #especificar forma
+             'rectangle':'rectangle', #especificar forma
+             'size':'size', #especificar tamanho
+             'height':'height', #especificar altura
+             'weight':'weight', #especificar largura
+             'point':'point'} #especificar ponto no plano
+output = []
